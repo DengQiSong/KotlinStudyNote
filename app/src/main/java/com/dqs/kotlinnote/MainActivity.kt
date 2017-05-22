6f7249b5151d0f678f97b8f85b1fa782471dfc61
邓启宋 <dengqs@physicalpoint.com>
2017-05-22 10:34:35 +0800

    Kotlin的int加运算

--- app/src/main/java/com/dqs/kotlinnote/MainActivity.kt
+++ app/src/main/java/com/dqs/kotlinnote/MainActivity.kt
@@ -15,8 +15,12 @@ class MainActivity : AppCompatActivity() {
         val next = findViewById(R.id.Button02) as Button
         next.setOnClickListener {
             Toast.makeText(this, "测试", Toast.LENGTH_SHORT).show()
-            firstName.setText("测试")
+            firstName.setText(sum(4, 5))
         }
-        firstName.setText("Hello World!")
+        firstName.setText(R.string.app_wod)
+    }
+
+    fun sum(a: Int, b: Int): String {
+        return (a + b).toString()
     }
 }
